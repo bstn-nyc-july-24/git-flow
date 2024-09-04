9c81693835812c3bb05ccf82a7d7f2456cdc9f9e
Eli Boninger <eboninger@brainstation.io>
2024-09-04 14:51:31 -0400

    Small copy change to NoConflictsHere

--- src/components/NoConflictsHere/NoConflictsHere.jsx
+++ src/components/NoConflictsHere/NoConflictsHere.jsx
@@ -3,7 +3,7 @@ import './NoConflictsHere.scss';
 const NoConflictsHere = () => {
     return (<div className="no-conflicts">
         <p className="no-conflicts__text">
-            Surely by naming the component <pre>&lt;NoConflictsHere&gt;</pre> we can avoid the possibility of merge conflicts. Suprised others have not thought of this NEAT trick.
+            Surely by naming the component <pre>&lt;NoConflictsHere&gt;</pre> we can avoid merge conflicts. Suprised others have not thought of this NEAT trick.
         </p>
 
     </div >);
