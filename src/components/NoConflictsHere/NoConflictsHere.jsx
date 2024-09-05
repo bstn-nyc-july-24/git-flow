3cf688ca689543cd2b873b56118adcda3cfbd432
Eli Boninger <eboninger@brainstation.io>
2024-09-05 14:03:05 -0400

    Update bem block to remove word "here"

--- src/components/NoConflictsHere/NoConflictsHere.jsx
+++ src/components/NoConflictsHere/NoConflictsHere.jsx
@@ -1,8 +1,8 @@
 import './NoConflictsHere.scss';
 
 const NoConflictsHere = () => {
-    return (<div className="no-conflicts-here">
-        <p className="no-conflicts-here__text">
+    return (<div className="no-conflicts">
+        <p className="no-conflicts__text">
             Surely by naming the component <pre>&lt;NoConflictsHere&gt;</pre> we can avoid the possibility of merge conflicts. Suprised others have not thought of this NEAT trick.
         </p>
 
